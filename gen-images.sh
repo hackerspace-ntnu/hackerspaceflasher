021388c71e5237c7a62ff420ee72a82c89b2b06b
Hackerspace <hackerspace@hackerspace>
2016-03-01 18:24:59 +0100

    - Prettify output

--- gen-images.sh
+++ gen-images.sh
@@ -3,6 +3,22 @@
 NUM_DEVICES=22
 HOSTNAME_PREFIX="B"
 
+echo "NTNU-Hackerspace Amazing Raspberry Pi Image Configuration ©2016"
+echo ""
+
+read -p "Write number of devices to configure: " NUM_DEVICES
+read -p "Hostname prefix: " HOSTNAME_PREFIX
+
+if [[ $NUM_DEVICES -lt 1 ]]; then
+echo "Usage: $0"
+echo ""
+echo "Example: $0"
+echo "  Write number of devices to configure: 20"
+echo "  Hostname prefix: B"
+echo ""
+	exit 1
+fi
+
 for i in $(seq 1 $NUM_DEVICES); do 
 	sudo mount -o loop,offset=1048576 ../raspbian.img ~/img
 	sudo bash -c "sed \"s|LOLID|${HOSTNAME_PREFIX}$i|g\" ../installer-config.txt > /home/hackerspace/img/installer-config.txt"
--- write-images.sh
+++ write-images.sh
@@ -2,7 +2,20 @@
 
 OUTDEV=/dev/sdc
 HOSTNAME_PREFIX="B"
-NUM_DEVICES=22
+NUM_DEVICES=20
+
+echo "NTNU-Hackerspace Amazing Raspberry Pi flasher ©2016"
+echo ""
+
+if [[ $NUM_DEVICES -lt 1 ]]; then
+echo "Usage: $0"
+echo ""
+echo "Example: $0 hello_world.zip"
+echo "  ..."
+	exit 1
+fi
+
+echo "Get ready to flash!"
 
 for i in $(seq 1 $NUM_DEVICES); do
 	read -p "Press any key to continue "
